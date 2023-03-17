66e85f9e3e55bc320e0c774d442152811f9f64dd
simon.lfr <simonryom@gmail.com>
2023-03-18 00:51:03 +0100

    Place on homepage.

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import styled from "styled-components";
 import { Button } from "../components/button/button";
 import { ColorLegend } from "../components/colorLegend/colorLegend";
+import { IconHeartOutlined } from "../components/icon/icon";
+import { IconExamples } from "../components/icon/iconExamples";
 import { HeadlineMedium } from "../components/typography/typography";
+import { useColorPalette } from "../providers/colorProvider";
 
 export function HomePage(): JSX.Element {
+  const colorPalette = useColorPalette();
+
   return (
     <Container>
+      <IconExamples />
       <div>
         <HeadlineMedium>Button</HeadlineMedium>
         <ButtonContainer>
           <Button variant="fill">
+            <IconHeartOutlined color={colorPalette.neutral.lightText} />
             <Button.text>Fill</Button.text>
           </Button>
           <Button variant="outline">
@@ -29,7 +36,11 @@ export function HomePage(): JSX.Element {
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
 const ButtonContainer = styled.div`
   display: flex;
   flex-direction: row;
